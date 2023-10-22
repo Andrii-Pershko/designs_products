cd6323c4044bd619b50eec407534ada18378f929
Andrii-Pershko <andriiinf@gmail.com>
2023-10-22 22:51:11 +0300

    move file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const adminCheck = require("./admin_check");
+const adminCheck = require("./middlewares/admin_check");
 const logger = require("morgan");
 const productsRouter = require("./routes/products");
 const app = express();
